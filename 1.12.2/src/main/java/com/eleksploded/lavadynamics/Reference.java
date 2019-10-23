a10098ee425d7910ed195d8e4a5e5e8b99e73db0
Eleksploded <pikachu9234@gmail.com>
2019-10-23 12:18:50 -0700

    Version Bump & Release

--- 1.12.2/src/main/java/com/eleksploded/lavadynamics/Reference.java
+++ 1.12.2/src/main/java/com/eleksploded/lavadynamics/Reference.java
@@ -3,7 +3,7 @@ package com.eleksploded.lavadynamics;
 public class Reference {
 	//Static values throughout the mod
 	public static final String MODID = "lavadynamics";
-	public static final String Version = "5.6.7";
+	public static final String Version = "5.6.8";
 	public static final String cproxy = "com.eleksploded.lavadynamics.proxy.CommonProxy";
 	public static final String sproxy = "com.eleksploded.lavadynamics.proxy.ServerProxy";
 }
