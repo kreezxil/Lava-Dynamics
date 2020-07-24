a313c3095d2a98ba8adee469599e5f6d21a871e2
Eleksploded <pikachu9234@gmail.com>
2020-07-23 20:58:00 -0700

    My brain hurts

--- src/main/java/com/eleksploded/lavadynamics/VolcanoManager.java
+++ src/main/java/com/eleksploded/lavadynamics/VolcanoManager.java
@@ -1,10 +1,21 @@
 package com.eleksploded.lavadynamics;
 
-import net.minecraftforge.event.world.ChunkEvent;
+import com.eleksploded.lavadynamics.cap.CheckedCap;
+import com.eleksploded.lavadynamics.cap.IChecked;
 
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.event.world.ChunkEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid= "lavadynamics")
 public class VolcanoManager {
 	
+	@SubscribeEvent
 	public static void chunkGen(ChunkEvent.Load e) {
-		e.getChunk();
+		Chunk chunk = ((Chunk)e.getChunk());
+		IChecked ch = chunk.getCapability(CheckedCap.checkedCap).orElseThrow(() -> new RuntimeException(CheckedCap.ThrowError));
+	
+		System.out.println(String.format("Chunk [%d, %d] is " + ch.isChecked(), chunk.getPos().x, chunk.getPos().z));
 	}
 }
